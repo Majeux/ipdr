7ed1c971bb8de7d221c09ce7d44ac5185c83acf5
Majeux <majeux@pop-os.localdomain>
2022-05-08 12:59:50 +0200

    unified literal creation and expression making

--- src/algo/bounded.cpp
+++ src/algo/bounded.cpp
@@ -13,7 +13,10 @@ namespace bounded
       : context(), graph(G), solver(context), lit_names(G.nodes),
         n_lits(lit_names.size())
   {
-    solver.add(initial());
+    ConstrainedExpr I = initial();
+    solver.add(I.expression);
+    solver.add(I.constraint);
+    current_bound = 0;
     bt_push(); // backtracking point to no transitions or cardinality
   }
 
@@ -23,25 +26,27 @@ namespace bounded
     return context.bool_const(full_name.c_str());
   }
 
-  expr Bounded::initial()
+  expr Bounded::constraint(const expr_vector& lits)
   {
-    expr_vector cube(context);
-    expr_vector lits(context);
+    return z3::atmost(lits, cardinality.value());
+  }
+
+  ConstrainedExpr Bounded::initial()
+  {
+    expr_vector cube(context), lits(context);
     for (string_view n : lit_names)
     {
       expr l = lit(n, 0);
       lits.push_back(l);
       cube.push_back(!l);
     }
-    cube.push_back(z3::atmost(lits, cardinality.value()));
 
-    return z3::mk_and(cube);
+    return { z3::mk_and(cube), constraint(lits) };
   }
 
-  expr_vector Bounded::final(size_t i)
+  ConstrainedExpr Bounded::final(size_t i)
   {
-    expr_vector cube(context);
-    expr_vector lits(context);
+    expr_vector cube(context), lits(context);
     for (string_view n : lit_names)
     {
       expr l = lit(n, i);
@@ -51,9 +56,8 @@ namespace bounded
       else
         cube.push_back(!l);
     }
-    cube.push_back(z3::atmost(lits, cardinality.value()));
 
-    return cube;
+    return { z3::mk_and(cube), constraint(lits) };
   }
 
   void Bounded::push_time_frame()
@@ -63,27 +67,18 @@ namespace bounded
     const size_t step = lits_at_time.size();
 
     for (std::string_view n : graph.nodes)
-    {
-      std::string name = fmt::format("{}[{}]", n, step);
-      z3::expr l       = context.bool_const(name.c_str());
-      v.push_back(l);
-    }
+      v.push_back(lit(n, step));
 
     lits_at_time.push_back(v);
   }
 
-  expr_vector Bounded::next_trans(size_t i)
+  ConstrainedExpr Bounded::trans_step(size_t i)
   {
-    expr_vector rv(context);
-    expr_vector current(context);
-    expr_vector next(context);
-
-    expr_vector T(context);
+    expr_vector next(context), T(context);
     for (std::string_view node : lit_names) // every node has a transition
     {
       expr source   = lit(node, i);
-      expr source_p = lit(node, i+1);
-      current.push_back(source);
+      expr source_p = lit(node, i + 1);
       next.push_back(source_p);
       // pebble if all children are pebbled now and next
       // or unpebble if all children are pebbled now and next
@@ -98,25 +93,27 @@ namespace bounded
       }
     }
 
-    rv.push_back(z3::mk_and(T));
-    rv.push_back(z3::atmost(next, cardinality.value()));
-    return rv;
+    return { z3::mk_and(T), constraint(next) };
   }
 
-  void Bounded::check(size_t steps)
+  z3::check_result Bounded::check(size_t steps)
   {
-    // fill transitions until steps-1
-    solver.check(final(steps));
+    push_transitions(steps);
+    z3::check_result result = solver.check(final(steps));
+    assert(result != z3::check_result::unknown);
+    return result;
   }
 
-  void Bounded::transition(size_t steps)
+  void Bounded::push_transitions(size_t steps)
   {
-    for (size_t i = 0; i < steps; i++)
+    for (size_t i = current_bound; i < steps; i++)
     {
-      expr_vector new_t = next_trans(i);
+      expr_vector new_t = trans_step(i);
       assert(new_t.size() == 2);
       solver.add(new_t);
     }
+
+    current_bound = steps - 1;
   }
 
   expr Bounded::iteration(size_t steps) {}
