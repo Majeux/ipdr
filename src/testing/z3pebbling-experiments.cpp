d62e09ad313eac261edc4b35b79465ad8790cdd5
Majeux <majeux@pop-os.localdomain>
2023-05-05 12:20:53 +0200

    fix seeds in z3pebbling experiment

--- src/testing/z3pebbling-experiments.cpp
+++ src/testing/z3pebbling-experiments.cpp
@@ -51,8 +51,7 @@ namespace pdr::test::experiments
       std::optional<unsigned> optimum;
       // new context with new random seed
       z3::context z3_ctx;
-      pdr::Context ctx(z3_ctx, args);
-      ctx.seed = seeds[i];
+      pdr::Context ctx(z3_ctx, args, seeds[i]);
 
       dag::Graph G = model_t::make_graph(ts_descr.src);
       Z3PebblingModel ts(args, z3_ctx, G);
