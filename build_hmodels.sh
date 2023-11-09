e29e616d61cd78cec115805345326f43a3d5f04b
Majeux <majeux@pop-os.localdomain>
2023-11-09 16:17:25 +0100

    remove old scripts (hop and old result collection)

--- build_hmodels.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-operators=("2,3" "3,4" "4,5" "5,7", "6,7" "8,7" "10,7" "12,7" "16,23")
-
-for op in "${operators[@]}"; do
-	command=(./pebbling-pdr --showonly "--hop=$op" 100)
-	echo "${command[@]}"
-	"${command[@]}"
-done
